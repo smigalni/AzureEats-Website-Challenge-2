161ac3a2ed8f24d76c2121260f3786e0d417fc87
Sergey <sergey.migalnikov@novanet.no>
2020-06-24 11:31:43 +0200

    Created terraform for Azure SQL

---
+++ Terraform/AzureSQL/variables.tf
@@ -0,0 +1,35 @@
+variable "sqlServerUser" {
+  description = "The administrator login user"
+}
+
+variable "sqlServerPassword" {
+  description = "The administrator login password"
+}
+
+variable "sqlServerName" {
+  description = "The sql server name"
+}
+
+variable "resourceGroupName" {
+  description = "The name of resource group"
+}
+
+variable "location" {
+  description = "Location"
+}
+
+variable "subscriptionId" {
+  description = "Subscription id"
+}
+
+variable "tenantId" {
+  description = "Tenant id"
+}
+
+variable "clientId" {
+  description = "Client id"
+}
+
+variable "clientSecret" {
+  description = "Client secret"
+}
